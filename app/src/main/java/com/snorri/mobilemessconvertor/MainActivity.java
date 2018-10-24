a77ddb686b5984ff18bcf8063a7819101cd5cc25
Snorri1986 <dionisiy1986@gmail.com>
2018-10-24 21:10:58 +0300

    Added General Activity. Added onClick method to Start button

--- app/src/main/java/com/snorri/mobilemessconvertor/MainActivity.java
+++ app/src/main/java/com/snorri/mobilemessconvertor/MainActivity.java
@@ -5,6 +5,7 @@ Date: 23.10.2018 00:26
 
 package com.snorri.mobilemessconvertor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -32,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
-        /**/
+
+
+        // Enter to General Activity
+        Button start_btn = (Button) findViewById(R.id.start_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGeneralActivity = new Intent(MainActivity.this,General.class);
+                startActivity(openGeneralActivity);
+            }
+        });
 
     }
 
